cb64aa153ecf810df7f801dd7cea41da24a36fb0
hoangda24312 <thanosligma942@gmail.com>
2026-04-14 13:54:13 +0700

    *

---
+++ DoanhNghiep_Group04/DoanhNghiep_Group04/Model/NhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoanhNghiep_Group04.Model
+{
+    internal class NhanVien
+    {
+        private string maNhanVien;
+        private string tenNhanVien;
+        private DateTime ngaySinh;
+        private string diaChi;
+        private int maBaoHiem;
+        private string maPhongBan;
+        private int maHopDong;
+        private bool trangThai;
+        private string maChucVu;
+        private string pathAnh;
+    }
+}
