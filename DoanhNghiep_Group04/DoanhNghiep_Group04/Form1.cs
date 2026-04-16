9a13fef6308ea617ce3ecdea29c0f76e40a35606
hoangda24312 <thanosligma942@gmail.com>
2026-04-16 22:42:45 +0700

    *

---
+++ DoanhNghiep_Group04/DoanhNghiep_Group04/Form1.cs
@@ -0,0 +1,10 @@
+namespace DoanhNghiep_Group04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DoanhNghiep_Group04/DoanhNghiep_Group04/Model/Database.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Text.Json;
+
+namespace DoanhNghiep_Group04.Model
+{
+
+    public class DbConfig
+    {
+        public string server { get; set; }
+        public string database_name { get; set; }
+        public string id { get; set; }
+        public string password { get; set; }
+        public string port { get; set; }
+        public string charset { get; set; }
+    }
+
+    internal class Database
+    {
+        private static string connectionString;
+
+        static Database()
+        {
+            string json = File.ReadAllText("database.json");
+            var db = JsonSerializer.Deserialize<DbConfig>(json);
+
+            connectionString = $"Server={db.server};Database={db.database_name};Uid={db.id};" +
+                               $"Pwd={db.password};Port={db.port};Charset={db.charset}";
+        }
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+
+}
