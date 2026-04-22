b2cb307dde23b33ccae200e567b6fcc6f100c87d
hoangda24312 <thanosligma942@gmail.com>
2026-04-22 21:28:28 +0700

    *

--- DoanhNghiep_Group04/DoanhNghiep_Group04/DangNhap.Designer.cs
+++ DoanhNghiep_Group04/DoanhNghiep_Group04/DangNhap.Designer.cs
@@ -28,95 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label2 = new Label();
-            label3 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DangNhap));
             btnDangNhap = new Button();
+            panel4 = new Panel();
+            pictureBox4 = new PictureBox();
+            panel5 = new Panel();
+            panel7 = new Panel();
+            textBox3 = new TextBox();
+            panel8 = new Panel();
+            panel3 = new Panel();
+            textBox2 = new TextBox();
+            panel6 = new Panel();
+            pictureBox1 = new PictureBox();
             panel1 = new Panel();
+            textBox1 = new TextBox();
+            panel2 = new Panel();
+            pictureBox2 = new PictureBox();
+            label4 = new Label();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel5.SuspendLayout();
+            panel7.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(512, 114);
-            label2.Name = "label2";
-            label2.Size = new Size(97, 25);
-            label2.TabIndex = 1;
-            label2.Text = "Username";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(512, 253);
-            label3.Name = "label3";
-            label3.Size = new Size(91, 25);
-            label3.TabIndex = 2;
-            label3.Text = "Password";
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = Color.Silver;
-            textBox1.BorderStyle = BorderStyle.None;
-            textBox1.Location = new Point(3, 15);
-            textBox1.Name = "textBox1";
-            textBox1.PlaceholderText = "Nhập tên đăng nhập";
-            textBox1.Size = new Size(242, 16);
-            textBox1.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(512, 292);
-            textBox2.Name = "textBox2";
-            textBox2.PlaceholderText = "Nhập mật khẩu";
-            textBox2.Size = new Size(248, 23);
-            textBox2.TabIndex = 4;
             // 
             // btnDangNhap
             // 
-            btnDangNhap.Location = new Point(512, 376);
+            btnDangNhap.BackColor = Color.DodgerBlue;
+            btnDangNhap.ForeColor = Color.Black;
+            btnDangNhap.Location = new Point(479, 336);
             btnDangNhap.Name = "btnDangNhap";
-            btnDangNhap.Size = new Size(248, 22);
+            btnDangNhap.Size = new Size(281, 42);
             btnDangNhap.TabIndex = 5;
             btnDangNhap.Text = "Đăng nhập";
-            btnDangNhap.UseVisualStyleBackColor = true;
+            btnDangNhap.UseVisualStyleBackColor = false;
+            btnDangNhap.Click += btnDangNhap_Click;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.DodgerBlue;
+            panel4.Controls.Add(pictureBox4);
+            panel4.Dock = DockStyle.Left;
+            panel4.Location = new Point(0, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(470, 450);
+            panel4.TabIndex = 9;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(12, 31);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(436, 398);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 14;
+            pictureBox4.TabStop = false;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.White;
+            panel5.Controls.Add(panel7);
+            panel5.Controls.Add(btnDangNhap);
+            panel5.Controls.Add(panel3);
+            panel5.Controls.Add(panel1);
+            panel5.Controls.Add(label4);
+            panel5.Dock = DockStyle.Fill;
+            panel5.Location = new Point(0, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(800, 450);
+            panel5.TabIndex = 10;
+            panel5.Paint += panel5_Paint;
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(textBox3);
+            panel7.Controls.Add(panel8);
+            panel7.Location = new Point(479, 278);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(278, 35);
+            panel7.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.White;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Location = new Point(0, 12);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(278, 16);
+            textBox3.TabIndex = 14;
+            textBox3.TextChanged += textBox3_TextChanged_1;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.DodgerBlue;
+            panel8.Location = new Point(0, 26);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(278, 10);
+            panel8.TabIndex = 13;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(panel6);
+            panel3.Controls.Add(pictureBox1);
+            panel3.Location = new Point(479, 159);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(278, 49);
+            panel3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.White;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(42, 22);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(230, 16);
+            textBox2.TabIndex = 14;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.DodgerBlue;
+            panel6.Location = new Point(-3, 44);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(278, 10);
+            panel6.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(39, 35);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
             // 
             // panel1
             // 
-            panel1.BackColor = Color.Silver;
             panel1.Controls.Add(textBox1);
-            panel1.Location = new Point(512, 160);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Location = new Point(476, 69);
             panel1.Name = "panel1";
-            panel1.Size = new Size(248, 46);
-            panel1.TabIndex = 6;
+            panel1.Size = new Size(278, 49);
+            panel1.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(45, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(230, 16);
+            textBox1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DodgerBlue;
+            panel2.Location = new Point(0, 44);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(275, 10);
+            panel2.TabIndex = 13;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(39, 35);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.FromArgb(243, 244, 246);
+            label4.ForeColor = Color.FromArgb(30, 64, 175);
+            label4.Location = new Point(560, 251);
+            label4.Name = "label4";
+            label4.Size = new Size(117, 24);
+            label4.TabIndex = 8;
+            label4.Text = "ABZyU";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            label4.Click += label4_Click;
             // 
             // DangNhap
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(btnDangNhap);
-            Controls.Add(textBox2);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(panel1);
+            Controls.Add(panel4);
+            Controls.Add(panel5);
             Name = "DangNhap";
             Text = "Form1";
+            panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel5.ResumeLayout(false);
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private Label label2;
-        private Label label3;
-        private TextBox textBox1;
-        private TextBox textBox2;
         private Button btnDangNhap;
+        private Panel panel4;
+        private Panel panel5;
+        private Label label4;
+        private PictureBox pictureBox2;
         private Panel panel1;
+        private Panel panel2;
+        private Panel panel7;
+        private Panel panel8;
+        private Panel panel3;
+        private Panel panel6;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox4;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
     }
 }
--- DoanhNghiep_Group04/DoanhNghiep_Group04/DangNhap.cs
+++ DoanhNghiep_Group04/DoanhNghiep_Group04/DangNhap.cs
@@ -6,5 +6,50 @@ namespace DoanhNghiep_Group04
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
